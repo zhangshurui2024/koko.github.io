601d869d7727990a8400c6385cc91b7a5c7e9ec4
zhangshurui2024 <2586313913@qq.com>
2024-08-20 20:54:44 +0800

    Site updated: 2024-08-20 20:54:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/18/hello-world/","2024/08/09/hexo博客部署AI摘要（TianLiGPT）教程/","2024/08/09/编程笔记本/","2024/08/10/部署轻笑前端/","2024/08/11/网站相关/"];function toRandomPost(){
+var posts=["2024/08/09/编程笔记本/","2024/08/18/hello-world/","2024/08/09/hexo博客部署AI摘要（TianLiGPT）教程/","2024/08/11/网站相关/","2024/08/10/部署轻笑前端/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
