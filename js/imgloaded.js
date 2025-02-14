d2d880679c8ab73195c913d4fa4a47ad54b060e2
Ray <2586313913@qq.com>
2025-02-14 19:24:38 +0800

    Site updated: 2025-02-14 19:24:13

--- js/imgloaded.js
+++ js/imgloaded.js
@@ -59,17 +59,17 @@
   
     const ldconfig = {
       light: {
-        smallSrc: 'img/bg2_80kbver.jpg', //浅色模式 小图链接 尽可能配置小于100k的图片 
-        largeSrc: 'img/bg2.jpg', //浅色模式 大图链接 最终显示的图片
-        mobileSmallSrc: 'img/bg2_80kbver.jpg', //手机端浅色小图链接 尽可能配置小于100k的图片
-        mobileLargeSrc: 'img/bg2.jpg', //手机端浅色大图链接 最终显示的图片
+        smallSrc: 'img/1bg2_80kbver.jpg', //浅色模式 小图链接 尽可能配置小于100k的图片 
+        largeSrc: 'img/1bg2.jpg', //浅色模式 大图链接 最终显示的图片
+        mobileSmallSrc: 'img/1bg2_80kbver.jpg', //手机端浅色小图链接 尽可能配置小于100k的图片
+        mobileLargeSrc: 'img/1bg2.jpg', //手机端浅色大图链接 最终显示的图片
         enableRoutes: ['/'],
         },
       dark: {
-        smallSrc: "img/bg1_80kbver.jpg", //深色模式 小图链接 尽可能配置小于100k的图片 
-        largeSrc: "img/bg1.jpg", //深色模式 大图链接 最终显示的图片
-        mobileSmallSrc: "img/bg1_80kbver.jpg", //手机端深色模式小图链接 尽可能配置小于100k的图片
-        mobileLargeSrc: "img/bg1.jpg", //手机端深色大图链接 最终显示的图片
+        smallSrc: "img/1bg1_80kbver.jpg", //深色模式 小图链接 尽可能配置小于100k的图片 
+        largeSrc: "img/1bg1.jpg", //深色模式 大图链接 最终显示的图片
+        mobileSmallSrc: "img/1bg1_80kbver.jpg", //手机端深色模式小图链接 尽可能配置小于100k的图片
+        mobileLargeSrc: "img/1bg1.jpg", //手机端深色大图链接 最终显示的图片
         enableRoutes: ['/'],
         },
       };
